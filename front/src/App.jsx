ed7824e93db4e09ff0ad35a0c21a6f188c37219a
Haneen Jamal Alsalman <haneenalsalman06@gmail.com>
2025-01-16 17:37:53 +0300

    Implement redirection to /signin on load

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes ,Navigate} from 'react-router-dom';
 import Signin from './auth/Signin';
 import Signup from './auth/Signup';
 import './index.css';
@@ -10,6 +10,7 @@ const App = () => {
 
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
         <Route path="/signup" element={<Signup />} /> 
         <Route path="/signin" element={<Signin />} />
         <Route path="/tasks" element={<Add />} />
